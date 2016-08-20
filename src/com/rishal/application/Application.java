322cd81fa1aa484970a2e9fb30d440968afb6091
Rishal Dev Singh <devrishal@gmail.com>
2016-08-20 21:14:09 +0530

    Modified Input taking part
    Modifying the input mechanism, where user is given option to quit

--- src/com/rishal/application/Application.java
+++ src/com/rishal/application/Application.java
@@ -7,32 +7,48 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import com.rishal.exception.InvalidSqlException;
+
 /**
  * Main Class used to launch the application.
+ * 
  * @author Rishal
  *
  */
 public class Application {
-	
-	public static void main(String[] args) throws IOException, InvalidSqlException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
-		//Taking Query input from user
-		LoadData a=LoadData.getInstance();
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-		System.out.println("CommandLine SQL- Enter the SQl query to get the result");
-		String inputQuery=br.readLine();
-		QueryType query=new QueryType();
-		QueryType.Type typeOfQuery = query.getLineType(inputQuery);
-		if(!typeOfQuery.equals(QueryType.Type.NOMATCH))
-		{
-			Class<?> c= Class.forName("com.rishal.queryOperation"+"."+typeOfQuery.toString());
-			Object t = c.newInstance();
-			Method method = c.getDeclaredMethod("display", String.class);
-			method.invoke(t, inputQuery);
-		}
-		else
-		{
-			throw new InvalidSqlException("Invalid Sql! Please enter valid SQL query.");
+
+	public static void main(String[] args) throws IOException,
+			InvalidSqlException, ClassNotFoundException,
+			InstantiationException, IllegalAccessException,
+			NoSuchMethodException, SecurityException, IllegalArgumentException,
+			InvocationTargetException {
+		// Taking Query input from user
+		LoadData a = LoadData.getInstance();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out
+				.println("CommandLine SQL- Enter the SQl query to get the result. Enter (Exit/Quit) to exit!");
+		String inputQuery = "";
+		while (!inputQuery.equalsIgnoreCase("Exit")
+				|| !inputQuery.equalsIgnoreCase("Quit")) {
+			inputQuery = br.readLine();
+			QueryType query = new QueryType();
+			QueryType.Type typeOfQuery = query.getLineType(inputQuery);
+			if (!typeOfQuery.equals(QueryType.Type.NOMATCH)) {
+				Class<?> c = Class.forName("com.rishal.queryOperation" + "."
+						+ typeOfQuery.toString());
+				Object t = c.newInstance();
+				Method method = c.getDeclaredMethod("display", String.class);
+				method.invoke(t, inputQuery);
+			} else if (!(inputQuery.equalsIgnoreCase("Exit"))
+					&& !(inputQuery.equalsIgnoreCase("Quit"))) {
+				throw new InvalidSqlException(
+						"Invalid Sql! Please enter valid SQL query.");
+			} else if (inputQuery.equalsIgnoreCase("Exit")
+					|| inputQuery.equalsIgnoreCase("Quit")) {
+				System.out.println("Sql Engine Closing !Bye");
+				break;
+
+			}
+
 		}
 
 	}
